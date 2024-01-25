3fb9c0d8921806a0f13fad89f05c4ecf8b5c6360
Aditya Gohil <89626455+Adi0015@users.noreply.github.com>
2024-01-25 17:35:52 +0530

    model development - speaker verification

--- SpeechRecognition.py
+++ SpeechRecognition.py
@@ -1,31 +1,63 @@
 import speech_recognition as sr
-import os
-# Initialize the recognizer
+import io
+import wave
+from google.cloud import speech_v1p1beta1 as speech
+from pydub import AudioSegment
+from pydub.playback import play
 
-def say(text):
-    os.system(f"say {text}")
+listener = sr.Recognizer()
 
-def takeCommand():
-    listener = sr.Recognizer()
-    with sr.Microphone() as source:
-        listener.pause_threshold = 0.7
-        # listener.energy_threshold = 4000
-        # listener.adjust_for_ambient_noise(source, duration=0.1)
-        # audio = listener.listen(source, timeout=None,phrase_time_limit=7)  # listen for the first phrase and extract it into audio data
-        audio = listener.listen(source)  # listen for the first phrase and extract it into audio data
-        x = listener.recognize_google(audio, language='eg-IN')  # recognize speech using Google Speech Recognition
-        query = x.lower()
-        #print(f"User said :{query}")
-    return query
 
-if __name__ == '__main__' :
+def micInput():
+    try:
+        with sr.Microphone() as source:  # ?! microphone as the audio source
+            # listener.pause_threshold = 0.4
+            listener.energy_threshold = 4000
+            listener.adjust_for_ambient_noise(source, duration=0.2)
+            # ! listen for the first phrase and extract it into audio data
+            audio = listener.listen(source, timeout=None, phrase_time_limit=10)
+            # ! recognize speech using Google Speech Recognition
+            x = listener.recognize_google(audio, language='eg-in')
+            command = x.lower()
+            if 'luna' in x.lower():
+              Verifcation(audio)
+            return  command
+            # print(command)
+    except Exception:  # ? speech is unintelligible
+        return ""
+
+
+def Verifcation(audio):
     
+    audio_data = audio.get_wav_data()
+    audio_buffer = io.BytesIO()
+    with wave.open(audio_buffer, "wb") as wf:
+        wf.setnchannels(1)
+        wf.setsampwidth(2)
+        wf.setframerate(45000)
+        wf.writeframes(audio_data)
+    audio_buffer.seek(0)
+    # play(audio_data)
+    print("step 1 done")
+    audio_segment = AudioSegment.from_wav(audio_buffer)
+    play(audio_segment)
+    
+        
+def extract_luna_audio(audio_data, keyword='luna'):
+    audio = AudioSegment(audio_data)
+
+    
+   
+    
+def lunaOutput():
+    micText = micInput()
+    if 'luna' in micText:
+        print(micText)
+
+def Luna():  # ! Main Function
     while True:
-        text=takeCommand()
-        #text=str(text)
-        if "hello luna" in text:
-            print("Hey , i am LUNA your Virtual Asisitant")
-            print("listening...")
-            text=takeCommand()
-            print(text)
-        break;
+        lunaOutput() # todo build all linked function
+
+if __name__ == '__main__':
+  Luna()
+
